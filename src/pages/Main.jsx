676a1af5976d88339be66d6dff19e9ba74104bba
HackISTP <hyeminpak@gmail.com>
2023-01-29 22:19:28 +0900

    :lipstick:	ADD: 버튼컴포넌트 적용

---
+++ src/pages/Main.jsx
@@ -0,0 +1,23 @@
+import Button from "../Common/Button";
+
+const Main = () => {
+  return (
+    <>
+      <h2>main</h2>
+      <Button
+        color="#FA7FD5"
+        bgcolor="#F9D2ED"
+        width="8rem"
+        height="2rem"
+        borderRadio=".5rem"
+        hoverBgColor=""
+        hoverWidth="10rem"
+        hoverHeight="4rem"
+        hoverTransition=" all 1s"
+      >
+        회원가입
+      </Button>
+    </>
+  );
+};
+export default Main;
